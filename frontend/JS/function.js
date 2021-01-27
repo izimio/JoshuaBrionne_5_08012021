862712e99d0a519dcf3039648d258f2b85a5d692
Joshua Brionne <Joshua.brionne@gmail.com>
2021-01-27 11:51:59 +0100

    fix index

--- frontend/JS/function.js
+++ frontend/JS/function.js
@@ -20,63 +20,8 @@ function refreshNumsAndPrice(){
     }
 }
 // +++++ GLOBAL +++++ //
-// ===== INDEX + GET / POST ===== //
 
-function GETIndex(value) {
-
-    fetch("http://localhost:3000/api/teddies/") 
-    .then(async result_ => { //GET the stringify tab
-        const response = await result_.json() //give a ame to that tab
-        tabAll = response; // getting the api's information inside my own variable
-        var index = -1;
-
-        // creating the price's tab
-        const allAPI = localStorage.getItem("TabAllPrice");
-        if(!allAPI){
-            var tab = [];
-            console.log('a');
-            while(response[++index]){
-                tab.push(tabAll[index].price / 100)
-            }
-            console.log(tab);
-            localStorage.setItem("TabAllPrice", JSON.stringify(tab))
-        }
-        index = -1;
-
-        while(response[++index]){
-            value.innerHTML += `
-        <div class="col-lg-4 col-sm-12 ">
-            <div class="productCard" id="productCard${index}">
-                <a href="../html/product_id=${tabAll[index]._id}.html">
-                <div class="productCard_img">
-                    <img src="${tabAll[index].imageUrl}" alt="photo de ${tabAll[index].name}">
-                </div>
-                <div class="productCard_caption">
-                    <div class="productCard_caption-upper">
-                        <div class="productCard_caption-upper-name">
-                            <h2>${tabAll[index].name}</h2>
-                        </div>
-                        <div class="productCard_caption-upper-price">
-                            <p>${tabAll[index].price / 100},00 €</p>
-                        </div>
-                    </div>
-                    <div class="productCard_caption-lower">
-                        <p class="productCard_caption-lower-description">${tabAll[index].description}</p>
-                    </div>
-                    <div class="productCard_caption-lower-addtocart">
-                        <p id="teddy${index}">En savoir plus</p>
-                    </div>
-                </div>
-                </a>
-            </div>
-        </div>`;
-        }
-    })
-    .catch((error) => {
-        console.error(error);
-    })
-};
-
+// ===== POST ===== //
 function POST(order) {
     fetch("http://localhost:3000/api/teddies/order", {
         method: 'POST',
--- frontend/JS/index.js
+++ frontend/JS/index.js
@@ -8,4 +8,51 @@ let nums = localStorage.getItem("PricesAndNums");
 refreshNums();
 
 // DISCARDING EVERY CARDS 
-GETIndex(main);
+fetch("http://localhost:3000/api/teddies/")
+    .then(async result_ => { //GET the stringify tab
+        const response = await result_.json() //give a ame to that tab
+        tabAll = response; // getting the api's information inside my own variable
+        var index = -1;
+
+        // creating the price's tab
+        const allAPI = localStorage.getItem("TabAllPrice");
+        if (!allAPI) {
+            var tab = [];
+            while (response[++index]) {
+                tab.push(tabAll[index].price / 100)
+            }
+            localStorage.setItem("TabAllPrice", JSON.stringify(tab))
+        }
+        index = -1;
+        while (response[++index]) {
+            main.innerHTML += `
+        <div class="col-lg-4 col-sm-12 ">
+            <div class="productCard" id="productCard${index}">
+                <a href="../html/product_id=${tabAll[index]._id}.html">
+                <div class="productCard_img">
+                    <img src="${tabAll[index].imageUrl}" alt="photo de ${tabAll[index].name}">
+                </div>
+                <div class="productCard_caption">
+                    <div class="productCard_caption-upper">
+                        <div class="productCard_caption-upper-name">
+                            <h2>${tabAll[index].name}</h2>
+                        </div>
+                        <div class="productCard_caption-upper-price">
+                            <p>${tabAll[index].price / 100},00 €</p>
+                        </div>
+                    </div>
+                    <div class="productCard_caption-lower">
+                        <p class="productCard_caption-lower-description">${tabAll[index].description}</p>
+                    </div>
+                    <div class="productCard_caption-lower-addtocart">
+                        <p id="teddy${index}">En savoir plus</p>
+                    </div>
+                </div>
+                </a>
+            </div>
+        </div>`;
+        }
+    })
+    .catch((error) => {
+        console.error(error);
+    })
